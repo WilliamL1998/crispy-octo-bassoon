a422401e9c54de345921d4b1b6450265607d78e2
T Omoto <tyler.omoto@gmail.com>
2022-07-04 13:26:29 -0400

    added routing and script for new review

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const newreview= require('./newreview');
 
 router.use('/users', userRoutes);
+router.use('/reviews', newreview);
 
 module.exports = router;
