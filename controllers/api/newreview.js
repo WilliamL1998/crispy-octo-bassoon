694e4059bc629a94f16825c91ded3e3ce464ed41
WilliamL1998 <williamliao1998@gmail.com>
2022-07-05 10:04:54 -0400

    Edited moviepage.handlebars

--- controllers/api/newreview.js
+++ controllers/api/newreview.js
@@ -7,7 +7,7 @@ router.post('/newreview', async (req, res) => {
         movie_id: req.body.movie_id,
         review: req.body.review,
         // using input user_id
-        user_id: req.body.user_id,
+        user_id: req.session.user_id,
         });
         res.status(200).json(newReviewData);
         // res.render('moviepage')
